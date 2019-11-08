a374eb204b1a5d72ae8f32e7e0d4bee2ea8bcbe4
wyb12 <1622027034@qq.com>
2019-11-08 14:52:53 +0800

    工具类

--- src/main/java/com/wuyubin/DateUtils.java
+++ src/main/java/com/wuyubin/DateUtils.java
@@ -23,7 +23,7 @@ public class DateUtils {
 	 */
 	public static int compare(Date date1,Date date2) {
 		if (date1==null || date2==null) {
-			throw new RuntimeException("参数不能为空111");
+			throw new RuntimeException("参数不能为空");
 		}
 		return date1.compareTo(date2);
 	}
