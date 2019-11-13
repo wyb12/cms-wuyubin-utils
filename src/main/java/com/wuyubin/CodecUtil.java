8f2c03a202a4b976face58f4da4690bc3198908b
wyb12 <1622027034@qq.com>
2019-11-13 08:46:18 +0800

    编码工具类

---
+++ src/main/java/com/wuyubin/CodecUtil.java
@@ -0,0 +1,48 @@
+package com.wuyubin;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author 吴宇斌
+ *
+ * 2019年11月13日
+ */
+public class CodecUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CodecUtil.class);
+
+    /**
+     * 将URL编码
+     */
+    public static String encodeURL(String source){
+        String target;
+        try {
+            target = URLEncoder.encode(source,"utf-8");
+        } catch (UnsupportedEncodingException e) {
+            LOGGER.error("encode url failure",e);
+            throw new RuntimeException(e);
+            //e.printStackTrace();
+        }
+        return target;
+    }
+
+    /**
+     * 将URL解码
+     */
+    public static String dencodeURL(String source){
+        String target;
+        try {
+            target = URLDecoder.decode(source,"utf-8");
+        } catch (UnsupportedEncodingException e) {
+            LOGGER.error("encode url failure",e);
+            throw new RuntimeException(e);
+            //e.printStackTrace();
+        }
+        return target;
+    }
+}
+
